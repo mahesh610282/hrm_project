32e6c32cbd543e788800110873fd5cd43d1efd3c
mahesh241994 <maheshcheerneni06@gmail.com>
2025-10-14 14:16:23 +0530

    my code is developed

--- hrmProject/myapp/views.py
+++ hrmProject/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def reigster(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        if password == confirm_password:
+            print(first_name, last_name, username, email, password)
+    return render(request, 'register.html')
